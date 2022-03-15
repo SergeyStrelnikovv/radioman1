8bf24c8c6c6b1412b4a10c4bf27f6f1164075582
SergeyStrelnikovv <94080285+SergeyStrelnikovv@users.noreply.github.com>
2022-03-15 21:44:10 +0300

    Update RadioService.java

--- src/main/java/ru/netology/radio/RadioService.java
+++ src/main/java/ru/netology/radio/RadioService.java
@@ -1,46 +1,57 @@
 package ru.netology.radio;
 
+
 public class RadioService {
-    private int currentVolume;
     private int currentStation;
+    private int stationsQuantity = 10;
+    private int currentVolume;
+
+    public RadioService() {
+    }
+
+    public RadioService(int stationsQuantity) {
+        this.stationsQuantity = stationsQuantity;
+    }
 
     public int getCurrentStation() {
         return currentStation;
+    }
+
+    public void setCurrentStation(int currentStation) {
+        if (currentStation < 0) return;
+        if (currentStation > stationsQuantity - 1) return;
+        this.currentStation = currentStation;
+    }
+
+    public void nextStation() {
+        if (currentStation == stationsQuantity - 1)
+            this.currentStation = 0;
+        else this.currentStation++;
+    }
+
+    public void prevStation() {
+        if (currentStation == 0)
+            this.currentStation = stationsQuantity - 1;
+        else this.currentStation--;
     }
 
     public int getCurrentVolume() {
         return currentVolume;
     }
 
-    public void setCurrentStation(int currentStation) {
-        if (currentStation < 0) return;
-        if (currentStation > 9) return;
-        this.currentStation = currentStation;
-    }
+    public void setCurrentVolume (int currentVolume){
+            if (currentVolume < 0) return;
+            if (currentVolume > 100) return;
+            this.currentVolume = currentVolume;
+        }
 
-    public void nextStation() {
-        if (currentStation == 9 ) this.currentStation = 0;
-        else this.currentStation++;
-    }
+        public void increaseVolume () {
+            if (currentVolume < 100)
+                this.currentVolume++;
+        }
 
-    public void prevStation() {
-        if (currentStation == 0) this.currentStation = 9;
-        else this.currentStation--;
-/*        if (currentStation <= 9) this.currentStation--;
-        if (currentStation < 0) this.currentStation = 9;*/
+        public void decreaseVolume () {
+            if (currentVolume > 0)
+                this.currentVolume--;
+        }
     }
-
-    public void setCurrentVolume(int currentVolume) {
-        if (currentVolume < 0) return;
-        if (currentVolume > 10) return;
-        this.currentVolume = currentVolume;
-    }
-
-    public void increaseVolume() {
-        if (currentVolume < 10) this.currentVolume++;
-    }
-
-    public void decreaseVolume() {
-        if(currentVolume > 0) this.currentVolume--;
-    }
-}
